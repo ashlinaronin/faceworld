fbf7ca0f76dc98008d5793966791b93cb92b7104
ashlinaronin <ashlin.aronin@pollinate.com>
2016-06-11 14:33:44 -0700

    Add vertex colors

--- src/services/CactusesService.js
+++ src/services/CactusesService.js
@@ -28,26 +28,6 @@
                 });
             }
 
-            // Create cactus and return a promise
-            // TODO: SLOW RIGHT NOW WE DONT NEED TO LOAD THIS THING MILLION TIMES
-            function _createCactus(size, position) {
-                var cactusDeferred = $q.defer();
-
-                objLoader.load('assets/cactus.obj', function(object) {
-                    object.traverse(function(child) {
-                        if (child instanceof THREE.Mesh) {
-                            child.material = cactusMaterial;
-                        }
-                    });
-                    object.scale.set(size, size, size);
-                    object.position.copy(position);
-
-                    cactusDeferred.resolve(object);
-                });
-
-                return cactusDeferred.promise;
-            }
-
             function _createBigCactus(size) {
                 objLoader.load('assets/cactus.obj', function(object) {
 
@@ -58,6 +38,7 @@
 										});
 
 										parentGeometry.scale(size, size, size);
+										parentGeometry = _colorBigCactus(parentGeometry);
 
 										var material = new THREE.MeshBasicMaterial({ vertexColors: THREE.VertexColors });
 										var mesh = new THREE.Mesh(parentGeometry, material);
@@ -66,31 +47,17 @@
                 });
             }
 
-            function _createCactuses(number, maxSize) {
-                for (var i = 0; i < number; i++) {
-                    var x = _getRandomInt(-maxSize, maxSize);
-                    var y = _getRandomInt(-maxSize, maxSize);
-                    var z = _getRandomInt(-maxSize, maxSize);
-                    var position = new THREE.Vector3(x, y, z);
+						function _colorBigCactus(cactus) {
+							cactus.faces.forEach(function(face) {
+								var color = new THREE.Color(0xffffff);
+								color.setHSL(Math.random(), Math.random(), Math.random());
+								face.color = color;
+							});
 
-                    var size = _getRandomInt(1, 15);
+							cactus.colorsNeedUpdate = true;
+							return cactus;
+						}
 
-                    var cactusPromise = _createCactus(size, position);
-                    cactusPromises.push(cactusPromise);
-                }
-
-                $q.all(cactusPromises).then(function(resolved) {
-                    // Resolve all cactuses promise with cactuses once we're
-                    // sure they've all been created
-                    allCactusesDeferred.resolve(resolved);
-                });
-            }
-
-            function _rotateCactuses(cactuses) {
-                for (var i = 0; i < cactuses.length; i++) {
-                    cactuses[i].rotation.y += (0.005 * i) + 0.005;
-                }
-            }
 
             /* Return a random integer between min (inclusive) and max (inclusive).
             ** Thanks to MDN. */
@@ -99,17 +66,9 @@
             }
 
             return {
-                getCactuses: function() {
-                    return allCactusesDeferred.promise;
-                },
                 getOneBigCactus: function() {
                     return bigCactusDeferred.promise;
-                },
-                rotateCactuses: _rotateCactuses
-            }
-
-
-
+                }
+						}
         }
-
 })();
